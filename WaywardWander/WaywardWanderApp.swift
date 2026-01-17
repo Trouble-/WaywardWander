9dc3cb99d853fa5c531669857101fe998c024608
JCM <jcmeik2@gmail.com>
2026-01-17 15:08:43 -0500

    Initial Commit

---
+++ WaywardWander/WaywardWanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaywardWanderApp.swift
+//  WaywardWander
+//
+//  Created by JCM on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct WaywardWanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
